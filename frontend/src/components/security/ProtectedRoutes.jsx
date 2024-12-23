ea32cdab280125cfc7956accd4148f54922c4e7e
Ben Thiam <seydinat235@gmail.com>
2024-12-23 12:56:36 +0000

    Mise en place du route proteger

--- frontend/src/components/security/ProtectedRoutes.jsx
+++ frontend/src/components/security/ProtectedRoutes.jsx
@@ -1,5 +1,14 @@
-const ProtectedRoutes = () => {
-  return <div>ProtectedRoutes</div>;
+/* eslint-disable react/prop-types */
+import { useAuthStore } from "../../zustand/store";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoutes = ({ children }) => {
+  const { token } = useAuthStore();
+  if (!token) {
+    return <Navigate to="/login" replace={true} />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoutes;
